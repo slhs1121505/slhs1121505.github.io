b23a445c3669f7f9f7ce70a93bf99e9b162708d1
PY癢癢的 <tzufu.taiwan@gmail.com>
2025-11-22 22:46:48 +0800

    Site updated: 2025-11-22 22:46:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/24/BLOG1024/","2025/10/19/APCS202510/","2025/09/30/CPEmediumship/","2025/08/17/HITCON2025/","2025/08/27/DP/","2025/09/13/oooo/","2025/09/22/touchgrass/","2025/11/21/NFU/","2025/08/19/Tips/"];function toRandomPost(){
+var posts=["2025/10/19/APCS202510/","2025/08/17/HITCON2025/","2025/10/24/BLOG1024/","2025/09/30/CPEmediumship/","2025/11/21/NFU/","2025/09/13/oooo/","2025/08/19/Tips/","2025/08/27/DP/","2025/09/22/touchgrass/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
